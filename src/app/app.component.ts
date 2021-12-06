762decb1105fb74afb6ef117c69a39c8e9f100c5
Michael Gerber <gerbermichi@me.com>
2021-12-06 08:49:37 +0100

    add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  date = new Date();
+  number = 1000000;
+  constructor(@Inject(LOCALE_ID) locale: any) {
+    console.log(locale);
+  }
 }

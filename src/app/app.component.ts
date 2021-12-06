56827d215dedc356c55c4fb24fef66493b39e25f
Michael Gerber <gerbermichi@me.com>
2021-12-06 08:58:51 +0100

    fix eslint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, LOCALE_ID} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,4 @@ import {Component, Inject, LOCALE_ID} from '@angular/core';
 export class AppComponent {
   date = new Date();
   number = 1000000;
-  constructor(@Inject(LOCALE_ID) locale: any) {
-    console.log(locale);
-  }
 }

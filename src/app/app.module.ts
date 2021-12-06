c1048c4ba2fa7de908eb22f7855221659c43494d
Michael Gerber <gerbermichi@me.com>
2021-12-06 08:57:18 +0100

    add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import {RippleModule} from "primeng/ripple";
 import {InputTextModule} from "primeng/inputtext";
 import {BadgeModule} from "primeng/badge";
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { StoreModule } from '@ngrx/store';
     RippleModule,
     InputTextModule,
     BadgeModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]

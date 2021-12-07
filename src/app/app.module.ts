d965f1ae6eebb6b41d45d8258eb209b6d47e2520
Michael Gerber <gerbermichi@me.com>
2021-12-07 16:15:01 +0100

    add initial oauth config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -11,6 +11,8 @@ import {BadgeModule} from "primeng/badge";
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
+import {AuthConfig, OAuthModule, OAuthService} from 'angular-oauth2-oidc';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -24,11 +26,42 @@ import {environment} from '../environments/environment';
     RippleModule,
     InputTextModule,
     BadgeModule,
+    HttpClientModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production})
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    OAuthModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      deps: [OAuthService],
+      useFactory: initializeAppFactory,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
+export function initializeAppFactory(oauthService: OAuthService): () => Promise<boolean> {
+  return () => {
+    oauthService.configure(authCodeFlowConfig);
+    return oauthService.loadDiscoveryDocumentAndLogin().then(loggedIn => {
+      if (loggedIn) {
+        oauthService.setupAutomaticSilentRefresh();
+        return loggedIn;
+      }
+      return Promise.reject("Not logged in");
+    });
+  };
+}
+
+
+export const authCodeFlowConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/auth/realms/appuio',
+  redirectUri: window.location.origin + '/index.html',
+  clientId: 'appuio-cloud-portal',
+  responseType: 'code',
+  scope: 'openid profile email',
+  showDebugInformation: !environment.production,
+};

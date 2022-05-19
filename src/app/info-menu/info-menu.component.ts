64160881b01287e5d0b551fa2eb00b37f27a29ab
Steffi Wehrle <stefanie.wehrle@nxt.engineering>
2022-05-19 15:18:40 +0200

    🔥 remove old references component & module as it's all in the info dropdown

--- src/app/info-menu/info-menu.component.ts
+++ src/app/info-menu/info-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
-import { Reference, ReferencesService } from '../core/references.service';
 
 @Component({
   selector: 'app-info-menu',
@@ -9,11 +8,45 @@ import { Reference, ReferencesService } from '../core/references.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoMenuComponent {
-  references: Reference[];
+  references: Reference[] = [
+    {
+      label: $localize`APPUiO Cloud Website`,
+      href: 'https://www.appuio.cloud',
+    },
+    {
+      label: $localize`Product Documentation`,
+      href: 'https://products.docs.vshn.ch/products/appuio/cloud/index.html',
+    },
+    {
+      label: $localize`Status Page`,
+      href: 'https://status.appuio.cloud',
+    },
+    {
+      label: $localize`Product Roadmap`,
+      href: 'https://roadmap.appuio.cloud',
+    },
+    {
+      label: $localize`Discussions and Help`,
+      href: 'https://discuss.appuio.cloud',
+    },
+    {
+      label: $localize`Community Chat`,
+      href: 'https://community.appuio.ch',
+    },
+    {
+      label: $localize`End-User Documentation`,
+      href: 'https://docs.appuio.cloud',
+    },
+    {
+      label: $localize`System Documentation`,
+      href: 'https://kb.vshn.ch/appuio-cloud',
+    },
+  ];
 
   faQuestion = faQuestionCircle;
+}
 
-  constructor(private refService: ReferencesService) {
-    this.references = refService.getReferences();
-  }
+export interface Reference {
+  label: string;
+  href: string;
 }

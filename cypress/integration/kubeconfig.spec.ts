258ec3e6fa12680edfd79c18bbaa229003829336
Steffi Wehrle <stefanie.wehrle@nxt.engineering>
2022-05-31 14:53:03 +0200

    ✅ fix test

--- cypress/integration/kubeconfig.spec.ts
+++ cypress/integration/kubeconfig.spec.ts
@@ -20,16 +20,17 @@ describe('Test kubeconfig', () => {
 
     cy.visit('/kubeconfig');
     cy.get('#kubeconfig-title').should('contain.text', 'Connect to APPUiO Control API');
-    const downloadBtn = cy.get('[title="Download Kubeconfig"]');
-    downloadBtn.should('have.attr', 'href');
-    downloadBtn.should('have.attr', 'download').then((filename) => {
-      downloadBtn.click();
-      // dispite what TS says, cy.should('have.attr') actually returns the attribute value as string and not an HTMLElement, hence this ugly cast
-      // https://docs.cypress.io/api/commands/should#Method-and-Value
-      cy.readFile(path.join(downloadsFolder, filename as unknown as string), { timeout: 15000 }).should(
-        'have.length.gt',
-        50
-      );
-    });
+    cy.get('[title="Download Kubeconfig"]').should('have.attr', 'href');
+    cy.get('[title="Download Kubeconfig"]')
+      .should('have.attr', 'download')
+      .then((filename) => {
+        cy.get('[title="Download Kubeconfig"]').click();
+        // dispite what TS says, cy.should('have.attr') actually returns the attribute value as string and not an HTMLElement, hence this ugly cast
+        // https://docs.cypress.io/api/commands/should#Method-and-Value
+        cy.readFile(path.join(downloadsFolder, filename as unknown as string), { timeout: 15000 }).should(
+          'have.length.gt',
+          50
+        );
+      });
   });
 });

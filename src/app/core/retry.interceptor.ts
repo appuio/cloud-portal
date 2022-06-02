5d68d084c65c119268b9e418e01ecea492c3a6fb
Steffi Wehrle <stefanie.wehrle@nxt.engineering>
2022-06-02 15:40:02 +0200

    🥅 retry failed calls with increasing delay

--- src/app/core/retry.interceptor.ts
+++ src/app/core/retry.interceptor.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable, retry } from 'rxjs';
+import { delay, Observable, of, retry } from 'rxjs';
 
 @Injectable()
 export class RetryInterceptor implements HttpInterceptor {
+  readonly MAX_RETRIES = 2;
+  readonly BASE_DELAY = 500;
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (request.method === 'GET') {
-      return next.handle(request).pipe(retry(1));
+      return next.handle(request).pipe(
+        retry({
+          count: this.MAX_RETRIES,
+          delay: (error, retryCount) => of(error).pipe(delay(this.BASE_DELAY * (retryCount - 1))),
+        })
+      );
     }
     return next.handle(request);
   }

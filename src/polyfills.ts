06bb4dcaacda81e0cc49497f565ed7ddab1c2729
ccremer <github.account@chrigel.net>
2023-04-18 14:50:32 +0200

    Add polyfill for structuredClone
    Some older browsers don't support structuredClone().

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,21 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/**
+ * structuredClone is somewhat recent, thus maybe not available in all browsers.
+ */
+(function () {
+  if (typeof structuredClone !== 'undefined') {
+    return;
+  }
+  function structuredClone<T>(obj: T): T {
+    const jsonString = JSON.stringify(obj);
+    return JSON.parse(jsonString);
+  }
+  if (typeof window !== 'undefined') {
+    window.structuredClone = structuredClone;
+  } else {
+    throw new Error('Could not attach structuredClone polyfill to the window object');
+  }
+})();
